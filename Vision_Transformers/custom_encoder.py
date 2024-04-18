005fa894369672980f3099d37700eebe1a6059e7
Prasoon Pandey <pandey.prasoon2000@gmail.com>
2024-04-18 23:49:07 +0200

    minor changes

--- Vision_Transformers/custom_encoder.py
+++ Vision_Transformers/custom_encoder.py
@@ -12,12 +12,15 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
 
 #=======================================================================================================#
+# Dataloaders
+
+#=======================================================================================================#
 # Highlight of Vision Transformers: Image splitting and embedding them!
 class PatchEmbedding(nn.Module):
     """
-    Splits images into patches and embed them
+    Splits images into patches, embed and flatten them as 1D vector!
     """
-    def __init__(self, img_size, patch_size, in_channels, embd_dim):
+    def __init__(self, img_size, patch_size, in_chans, embed_dim):
         super().__init__()
         self.img_size = img_size
         self.patch_size = patch_size
@@ -25,8 +28,8 @@ class PatchEmbedding(nn.Module):
 
         # splitting images into patches 
         self.proj = nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=embd_dim,
+            in_channels=in_chans,
+            out_channels=embed_dim,
             kernel_size=patch_size,
             stride=patch_size,
         )
@@ -34,6 +37,7 @@ class PatchEmbedding(nn.Module):
     def forward(self, x):
         """
         Run forward pass. 
+        To feed my images into the transformer. I have to flatten out each patch with shape (196, 768) where 196 is total number of patches and 768 is embedding dimension.
         """
         # x: (n_samples, in_channels, img_size, img_size)
         x = self.proj(x) # (n_samples, embd_dim, num_patches**0.5, num_patches**0.5)
@@ -48,11 +52,11 @@ class AttentionModule(nn.Module):
     """
     Block to implement the attention mechanism 
     """
-    def __init__(self, dim, num_heads=10, qkv_bias=False, attn_dpp=0.2, proj_dpp=0.2):
+    def __init__(self, dim, n_heads=10, qkv_bias=True, attn_p=0., proj_p=0.):
         super().__init__()
-        self.num_heads = num_heads
+        self.n_heads = n_heads
         self.dim = dim # channels
-        self.head_dim = (dim // num_heads)
+        self.head_dim = (dim // n_heads)
 
         # creating query, key and value for each token in a different manner
         self.qkv = nn.Linear(
@@ -60,9 +64,9 @@ class AttentionModule(nn.Module):
             out_features=dim*3, 
             bias=qkv_bias
         )
-        self.attn_drop = nn.Dropout(attn_dpp)
+        self.attn_drop = nn.Dropout(attn_p)
         self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(proj_dpp)
+        self.proj_drop = nn.Dropout(proj_p)
 
     def forward(self, x):
         """
@@ -78,7 +82,7 @@ class AttentionModule(nn.Module):
         qkv = self.qkv(x) # (n_samples, num_patches + 1, 3*dim)
         # create extra dimension for num_heads and key, query, value
         qkv = qkv.reshape(
-            n_samples, num_tokens, 3, self.num_heads, self.head_dim
+            n_samples, num_tokens, 3, self.n_heads, self.head_dim
         )
         # change the order
         qkv = qkv.permute(2, 0, 3, 1, 4) # (3, n_samples, num_heads, num_tokens, head_dim)
@@ -104,7 +108,7 @@ class AttentionModule(nn.Module):
 # Create the feedforward network or MLP
 class FeedForwardNet(nn.Module):
 
-    def __init__(self, in_features, hidden_features, out_features, dpp=0.2):
+    def __init__(self, in_features, hidden_features, out_features, p=0.):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(in_features=in_features, out_features=hidden_features),
@@ -112,7 +116,7 @@ class FeedForwardNet(nn.Module):
             nn.Linear(in_features=hidden_features, out_features=out_features)
         )
 
-        self.dropout = nn.Dropout(dpp)
+        self.dropout = nn.Dropout(p)
 
     def forward(self, x):
         x = self.net(x)
@@ -124,18 +128,18 @@ class VTransformerBlock(nn.Module):
     """
     Individual encoder block for the vision transformer
     """
-    def __init__(self, dim, num_heads, qkv_bias=False, attn_dpp=0.2, dpp=0.2): # dim: embedding dimension
+    def __init__(self, dim, n_heads, mlp_ratio=4.0, qkv_bias=True, p=0., attn_p=0.): # dim: embedding dimension
         super().__init__()
         self.self_attn = AttentionModule(
             dim=dim,
-            num_heads=num_heads,
+            n_heads=n_heads,
             qkv_bias=qkv_bias,
-            attn_dpp=attn_dpp, 
-            proj_dpp=dpp
+            attn_p=attn_p, 
+            proj_p=p
         )
         self.ffwd = FeedForwardNet(
             in_features=dim, 
-            hidden_features=4*dim, 
+            hidden_features=mlp_ratio*dim, 
             out_features=dim
         )
         self.layer_norm_1 = nn.LayerNorm(dim)
@@ -159,14 +163,15 @@ class VisionTransformer(nn.Module):
             self, 
             img_size=384,
             patch_size=16, # number of patches = 24 (for img: 384 and patch: 16)
-            in_channels=3,
+            in_chans=3,
             n_classes=1000,
-            embd_dim=768,
+            embed_dim=768, # each patch is 16x16 with 3 channel. therefore, 16x16x3=768
             depth=12, # no of encoders
-            num_heads=12,
-            qkv_bias=False,
-            attn_dpp=0.2,
-            dpp=0.2
+            n_heads=12,
+            qkv_bias=True,
+            mlp_ratio=4.0,
+            attn_p=0.,
+            p=0.
         ):
         super().__init__()
 
@@ -174,32 +179,33 @@ class VisionTransformer(nn.Module):
         self.patch_embd = PatchEmbedding(
             img_size=img_size,
             patch_size=patch_size,
-            in_channels=in_channels,
-            embd_dim=embd_dim
+            in_chans=in_chans,
+            embed_dim=embed_dim
         )
 
         # include the class token
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, embd_dim))
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
         # position embedding
-        self.pos_embd = nn.Parameter(torch.zeros(1, 1 + self.patch_embd.num_patches, embd_dim))
-        self.pos_drop = nn.Dropout(p=dpp)
+        self.pos_embd = nn.Parameter(torch.zeros(1, 1 + self.patch_embd.num_patches, embed_dim))
+        self.pos_drop = nn.Dropout(p=p)
 
         self.blocks = nn.ModuleList(
             [
             VTransformerBlock(
-                dim=embd_dim, 
-                num_heads=num_heads,
+                dim=embed_dim, 
+                n_heads=n_heads,
+                mlp_ratio=mlp_ratio,
                 qkv_bias=qkv_bias,
-                attn_dpp=attn_dpp,
-                dpp=dpp
+                attn_p=attn_p,
+                p=p
             )
             for _ in range(depth)
             ]
         )
 
-        self.norm = nn.LayerNorm(embd_dim)
+        self.norm = nn.LayerNorm(embed_dim)
         # Linear projection to get the probablities for each class
-        self.head = nn.Linear(embd_dim, n_classes)
+        self.head = nn.Linear(embed_dim, n_classes)
 
     def forward(self, x):
         """
